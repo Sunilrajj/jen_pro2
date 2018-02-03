10243e6bc17961bbd5bc47a8972fb53698ed7347
Sunil Raj <35327168+Sunilrajj@users.noreply.github.com>
2018-02-03 15:33:48 +0530

    Update Aa.java

--- src/main/java/pro2/Aa.java
+++ src/main/java/pro2/Aa.java
@@ -1,14 +1,9 @@
 package pro2;
 
-import ponmurugan.ponmurugan.Joda;
-
 public class Aa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Joda j=new Joda();
 System.out.println("current date");
-j.getDate();
 
 	}
 
